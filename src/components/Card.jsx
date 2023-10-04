12c365180fe5eb8c066f19df59013a6c36158980
Ayoub_oulaarbi <ayoub6oulaarbi6@gmail.com>
2023-10-04 23:44:43 +0100

    added some styling using tailwindcss classes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='max-w-xs bg-white border border-gray-200 '>
+    <div className='max-w-xs mt-4 bg-white border border-gray-200 '>
         <img src="https://images.squarespace-cdn.com/content/v1/54d4153ce4b00c0e483c13a6/1666007364921-OS0GBEDR3TLEU2DREDVQ/zaalouk%2Bdip.jpg?format=750w"
         alt="imgg" className='p-8 rounded-t-lg' />
-        <span>Zalouk</span>
-        <span>Recipe by cha9or</span>
+        <div className='text-center'>
+          <span className='font-bold mx-4'>Zaalouk</span>
+          <span>Recipe by someChef</span>
+        </div>
     </div>
   )
 }
