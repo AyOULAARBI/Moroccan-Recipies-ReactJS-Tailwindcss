60e3c126ca5d779cb487fc09dfdd45205c5e68a4
Ayoub_oulaarbi <ayoub6oulaarbi6@gmail.com>
2023-10-04 23:44:59 +0100

    created the main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,41 @@
+import Card from "./Card"
+
+
+function Main() {
+  return (
+    <main className='px-14 py-6'>
+        <div>
+          <a href="#">Login</a>
+          <a href="#">Sign Up</a>
+        </div>
+
+        <header>
+          <h2 className='text-gray-900 text-5xl font-semibold'>Recipes</h2>
+          <h3 className='text-gray-900 text-xl font-semibold'>For foreigners</h3>
+        </header>
+
+        <div>
+          <h3 className='font-bold mt-8'>Latest Recipes</h3>
+          <div className='flex'>
+            {/* cards */}
+            <Card />
+            <Card />
+            <Card />
+
+          </div>
+        </div>
+
+          <h3 className='font-bold mt-8'>Most popular Recipes</h3>
+          <div>
+            {/* cards */}
+            <Card/>
+          </div>
+
+          <div>
+            <div>Load More</div>
+          </div>
+      </main>
+  )
+}
+
+export default Main
